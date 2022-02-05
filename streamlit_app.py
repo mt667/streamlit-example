d4473637642928613ad264e446d2e4169d798845
mt667 <mt667@cornell.edu>
2022-02-05 02:37:01 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,23 @@
 from collections import namedtuple
 import altair as alt
-import math
+import numpy as np
 import pandas as pd
 import streamlit as st
 
-"""
-# Welcome to Streamlit!
+#Homework 1: Deleted the starter code and am now writing my own code below
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+st.title('Homework 1 Github and Streamlit')
+st.write('By Mariya Tasnim')
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+st.write('I was thinking of a project where we help students create the best school class-free time schedule for themselves')
+#st.write('Below is a sample schedule students may have')
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+st.metric(label="Student 1", value="8-8:50 AM", delta="10% likelihood to attend")
+col1, col2, col3 = st.columns (3)
+col1.metric("Student 1", "8-8:50AM", "10% likelihood to attend")
+col2.metric("Student 2", "8-8:50AM", "30% likelihood to attend")
+col3.metric("Student 3", "8-8:50AM", "50% likelihood to attend")
 
-
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+#array=np.array
+#columns=('student $d' %for i in range (3))
+#st.table()
